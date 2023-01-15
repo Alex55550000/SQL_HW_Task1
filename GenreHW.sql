9f1e40d098eb9bd9530d9a54ab328461c40c7732
Alex55550000 <106884796+Alex55550000@users.noreply.github.com>
2023-01-15 13:48:36 +0300

    Update GenreHW.sql

--- GenreHW.sql
+++ GenreHW.sql
@@ -1,35 +1,35 @@
 CREATE TABLE `album`(
-    `Album_ID` INT NOT NULL,
-    `Album_name` VARCHAR(255) NOT NULL,
-    `Album_year_of_issue` DATE NOT NULL
+    `Album_ID` SERIAL PRIMARY KEY,
+    `Album_name` VARCHAR(60),
+    `Album_year_of_issue` INT NOT NULL
 );
 CREATE TABLE `author`(
-    `Author_ID` INT NOT NULL,
-    `Author_name` VARCHAR(255) NOT NULL
+    `Author_ID` SERIAL PRIMARY KEY,
+    `Author_name` VARCHAR(60)
 );
 CREATE TABLE `authoralbum`(
     `Author_ID` INT NOT NULL,
-    `Album_ID` INT NOT NULL
+    `Album_ID` INT REFERENCES 'album(Album_ID)'
 );
 CREATE TABLE `collection`(
-    `Collection_ID` INT NOT NULL,
-    `Collection_name` VARCHAR(255) NOT NULL,
-    `Collection_year_of_issue` DATE NOT NULL
+    `Collection_ID` SERIAL PRIMARY KEY,
+    `Collection_name` VARCHAR(60),
+    `Collection_year_of_issue` INT NOT NULL
 );
 CREATE TABLE `collectiontracks`(
-    `Tracks_ID` INT NOT NULL,
-    `Collection_ID` INT NOT NULL
+    `Tracks_ID` INT REFERENCES 'tracks(Tracks_ID)',
+    `Collection_ID` INT REFERENCES 'collection(Collection_ID)'
 );
 CREATE TABLE `genre`(
-    `Genre_ID` INT NOT NULL,
-    `Genre_name` VARCHAR(255) NOT NULL
+    `Genre_ID` SERIAL PRIMARY key,
+    `Genre_name` VARCHAR(60)
 );
 CREATE TABLE `genreauthor`(
-    `Genre_ID` INT NOT NULL,
-    `Author_ID` INT NOT NULL
+    `Genre_ID` INT REFERENCES 'genre(Genre_ID)',
+    `Author_ID` INT REFERENCES 'author(Author_ID)'
 );
 CREATE TABLE `tracks`(
-    `Tracks_ID` INT NOT NULL,
-    `Track_name` VARCHAR(255) NOT NULL,
-    `Track_duration` VARCHAR(255) NOT NULL
+    `Tracks_ID` SERIAL PRIMARY KEY,
+    `Track_name` VARCHAR(60),
+    `Track_duration` INT NOT NULL
 );
